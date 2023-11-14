31ed642fab471608d1ce72eb6c78e44865d2ab82
José <josepsousa2012@gmail.com>
2023-11-14 10:16:37 -0300

    changes

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 import multiprocessing
 import os
 
-os.environ.setdefault("PORT", "8081")
+os.environ.setdefault("PORT", "8080")
 bind = "0.0.0.0:" + f"{os.environ.get('PORT', 8080)}"
 workers = multiprocessing.cpu_count()
 # worker_class = "gevent"
