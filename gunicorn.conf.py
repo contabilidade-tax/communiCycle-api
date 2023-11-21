d02b57222ff39e638800ee5461317aea8a511c6a
José <josepsousa2012@gmail.com>
2023-11-21 17:01:36 -0300

    merda de porta do heroku

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 import multiprocessing
 import os
 
-bind = f"0.0.0.0:{os.environ.get('API_PORT', 8080)}"
+bind = f"0.0.0.0:{os.environ.get('API_PORT', os.environ.get('PORT', 8080))}"
 workers = multiprocessing.cpu_count()
 # worker_class = "gevent"
 timeout = 120
